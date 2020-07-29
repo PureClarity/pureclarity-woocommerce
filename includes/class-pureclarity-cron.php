482a907875fae8f199d81a7e45b6273c3466c207
Gareth James <gareth.james@pureclarity.com>
2020-07-29 12:33:19 +0100

    improving category delta so that if an exception is thrown it's caught in it's own funciton

--- includes/class-pureclarity-cron.php
+++ includes/class-pureclarity-cron.php
@@ -173,25 +173,23 @@ class PureClarity_Cron {
 	 * Processes a category delta
 	 */
 	public function process_categories() {
+		try {
+			if ( ! $this->settings->is_category_feed_sent() ) {
+				return;
+			}
 
-		if ( ! $this->settings->is_category_feed_sent() ) {
-			return;
-		}
+			if ( ! empty( $this->settings->get_category_feed_required() ) ) {
+				$this->settings->clear_category_feed_required();
 
-		if ( ! empty( $this->settings->get_category_feed_required() ) ) {
-
-			$this->settings->clear_category_feed_required();
-
-			$data = $this->feed->build_items( 'category', 1 );
-			if ( ! empty( $data ) ) {
-				try {
+				$data = $this->feed->build_items( 'category', 1 );
+				if ( ! empty( $data ) ) {
 					$this->feed->start_feed( 'category' );
 					$this->feed->send_data( 'category', $data );
 					$this->feed->end_feed( 'category' );
-				} catch ( \Exception $exception ) {
-					error_log( 'PureClarity: An error occurred updating categories: ' . $exception->getMessage() );
 				}
 			}
+		} catch ( \Exception $exception ) {
+			error_log( 'PureClarity: An error occurred updating categories: ' . $exception->getMessage() );
 		}
 	}
 
