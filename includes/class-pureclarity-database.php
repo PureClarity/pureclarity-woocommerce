bfcf56c68a5ebbff65c8714966b7264ccf238af4
Gareth James <gareth.james@pureclarity.com>
2020-09-02 10:22:41 +0100

    Fixing WordPress coding standards issues

--- includes/class-pureclarity-database.php
+++ includes/class-pureclarity-database.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * Handles Database upgrades
+ * Handles Database upgrades for PureClarity tables
  */
 class PureClarity_Database {
 
@@ -17,9 +17,9 @@ class PureClarity_Database {
 	 * @param string $current_db_version - the order id to get info for.
 	 */
 	public function update_db( $current_db_version ) {
-		if ($current_db_version < PURECLARITY_DB_VERSION) {
-			for ($i = $current_db_version; $i < PURECLARITY_DB_VERSION; $i++) {
-				$f = 'update_db_to_' . ($i + 1);
+		if ( $current_db_version < PURECLARITY_DB_VERSION ) {
+			for ( $i = $current_db_version; $i < PURECLARITY_DB_VERSION; $i++ ) {
+				$f = 'update_db_to_' . ( $i + 1 );
 				if ( method_exists( $this, $f ) ) {
 					$this->$f();
 				}
@@ -27,6 +27,9 @@ class PureClarity_Database {
 		}
 	}
 
+	/**
+	 * Upgrades to version1 of the PureClarity Database table structure, creating the delta & state tables
+	 */
 	public function update_db_to_1() {
 		$this->create_state_table();
 		$this->create_delta_table();
@@ -36,38 +39,36 @@ class PureClarity_Database {
 	/**
 	 * Creates the pureclarity_state table
 	 */
-	public function create_state_table()
-	{
+	public function create_state_table() {
 		global $wpdb;
 
-		$table_name = $wpdb->prefix . 'pureclarity_state';
+		$table_name      = $wpdb->prefix . 'pureclarity_state';
 		$charset_collate = $wpdb->get_charset_collate();
-		$sql = "CREATE TABLE $table_name (
+		$sql             = "CREATE TABLE $table_name (
   			`name` CHAR(35) NOT NULL,
   			`value` TEXT NOT NULL,
   			UNIQUE INDEX `pureclarity_unique_name` (`name` ASC));
 		) $charset_collate COMMENT 'PureClarity State Table - Stores key information about the state of the PureClarity integration';";
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		dbDelta( $sql );
 	}
 
 	/**
 	 * Creates the pureclarity_state table
 	 */
-	public function create_delta_table()
-	{
+	public function create_delta_table() {
 		global $wpdb;
 
-		$table_name = $wpdb->prefix . 'pureclarity_delta';
+		$table_name      = $wpdb->prefix . 'pureclarity_delta';
 		$charset_collate = $wpdb->get_charset_collate();
-		$sql = "CREATE TABLE $table_name (
+		$sql             = "CREATE TABLE $table_name (
   			`type` CHAR(12) NOT NULL,
   			`id` integer UNSIGNED NOT NULL,
   			UNIQUE INDEX `pureclarity_unique_name` (`type` ASC, `id` ASC));
 		) $charset_collate COMMENT 'PureClarity Delta Table - Stores ids of enities that need to be sent ot PureClarity as deltas';";
 
-		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 		dbDelta( $sql );
 	}
 
