9ef2f50e4c39d933fdd62efec533e7ed08003e2c
Gareth James <gareth.james@pureclarity.com>
2020-09-03 17:51:52 +0100

    Changed plugin init so cron always gets scheduled correctly, and public class doesnt get called on cron / rest api calls

--- includes/class-pureclarity-plugin.php
+++ includes/class-pureclarity-plugin.php
@@ -38,18 +38,38 @@ class PureClarity_Plugin {
 	 */
 	public function init() {
 		$this->load_dependencies();
+
+		// this just schedules cron, needs to be always run to ensure scheduling happens.
+		$cron = $this->loader->get_cron();
+		$cron->init();
+
+		// watchers always need to be run, so that we can pick up on any events in frontend / backend.
+		$watcher = $this->loader->get_products_watcher();
+		$watcher->init();
+
 		if ( is_admin() ) {
 			$admin = $this->loader->get_admin();
 			$admin->init();
-		} elseif ( defined( 'DOING_CRON' ) ) {
-			$cron = $this->loader->get_cron();
-			$cron->init();
-		} elseif ( ! wp_doing_ajax() ) {
+		} elseif ( ! defined( 'DOING_CRON' ) && ! wp_doing_ajax() && ! $this->is_rest_api_request() ) {
 			$public = $this->loader->get_public();
 			$public->init();
 		}
+	}
 
-		$watcher = $this->loader->get_products_watcher();
-		$watcher->init();
+	/**
+	 * Detects if this is a WordPress API request, repurposed from WooCommerce's own version of this function.
+	 * See https://wordpress.stackexchange.com/a/356946
+	 *
+	 * @return false|mixed|void
+	 */
+	public function is_rest_api_request() {
+		if ( empty( $_SERVER['REQUEST_URI'] ) ) {
+			return false;
+		}
+
+		$rest_prefix         = trailingslashit( rest_get_url_prefix() );
+		$is_rest_api_request = ( false !== strpos( $_SERVER['REQUEST_URI'], $rest_prefix ) );
+
+		return apply_filters( 'pureclarity_is_rest_api_request', $is_rest_api_request );
 	}
 }
